191fdc690f23666db2e1aef791edd73b090a2e33
richardhenyash <richardash1@gmail.com>
2022-05-11 09:08:06 +0100

    add menu and app functions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from models import (
     Base, session,
     Book, engine)
-    
+
 # main menu - add, search, analysis, exit, view
 # add books to the database
 # edit books
@@ -10,5 +10,51 @@ from models import (
 # data cleaning
 # loop runs program
 
+def menu():
+    while True:
+        print(
+            '''
+            \nPROGRAMMING BOOKS
+            \r1) Add book
+            \r2) View all books
+            \r3) Search for book
+            \r4) Book analysis
+            \r5) Exit
+            ''')
+        choice = input('What would you like to do? ')
+        if choice in ('1', '2', '3', '4', '5'):
+            return choice
+        else:
+            input (
+                '''
+                \rPlease choose one of the options above.
+                \rA number from 1 to 5.
+                \rPress enter to try again.
+                '''
+                )
+
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+        if choice == '1':
+            # add book
+            pass
+        elif choice =='2':
+            # view books
+            pass
+        elif choice =='3':
+            # search for book
+            pass
+        elif choice =='4':
+            # book analysis
+            pass
+        else:
+            print('GOODBYE')
+            app_running = False
+
+
+
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    app()
