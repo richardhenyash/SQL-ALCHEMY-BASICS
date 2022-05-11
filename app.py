421881a48b73ae93f85e1d7ce20d114dd4d1e962
richardhenyash <richardash1@gmail.com>
2022-05-11 08:49:27 +0100

    added database model for books database

---
+++ app.py
@@ -0,0 +1,14 @@
+from models import (
+    Base, session,
+    Book, engine)
+    
+# main menu - add, search, analysis, exit, view
+# add books to the database
+# edit books
+# delete books
+# search books
+# data cleaning
+# loop runs program
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
--- models.py
+++ models.py
@@ -1,36 +1,23 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import (
+    create_engine, Column,
+    Integer, String, Date)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///users.db', echo=False)
+engine = create_engine('sqlite:///books.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__  = 'users'
+class Book(Base):
+    __tablename__  = 'books'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
+    title = Column('Title', String)
+    author = Column('Author', String)
+    published_date = Column('Published', Date)
+    price = Column('Price', Integer)
 
     def __repr__(self):
-        return f'<User(name={self.name}, fullname={self.fullname}, nickname={self.nickname})>'
-
-if __name__ == '__main__':
-    Base.metadata.create_all(engine)
-
-    richard_user = User(name='Richard', fullname='Richard Ash', nickname='Rich')
-    print(richard_user.name)
-    print(richard_user.id)
-    session.add(richard_user)
-    new_users = ([
-        User(name='Grace', fullname='Grace Hopper', nickname='Pioneer'),
-        User(name='Alan', fullname='Alan Turing', nickname='Computer Scientist'), 
-        User(name='Katherine', fullname='Katherine Johnson', nickname='')
-    ])
-    session.add_all(new_users)
-    session.commit()
-
+        return f'Title: {self.title} Author: {self.author} Published: {self.published_date} Price: {self.price}'
